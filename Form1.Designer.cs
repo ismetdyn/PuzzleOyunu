c1ea51a4685337b48e908a87306721631969d893
ismet <ismet@ABRAA5>
2022-02-23 17:20:00 +0300

    Proje dosyası ekle.

---
+++ Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿
+namespace Proje5._36_PuzzleOyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbAlan = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbAlan)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbAlan
+            // 
+            this.pbAlan.BackColor = System.Drawing.Color.White;
+            this.pbAlan.Location = new System.Drawing.Point(63, 22);
+            this.pbAlan.Name = "pbAlan";
+            this.pbAlan.Size = new System.Drawing.Size(100, 50);
+            this.pbAlan.TabIndex = 0;
+            this.pbAlan.TabStop = false;
+            this.pbAlan.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pbAlan_MouseDown);
+            this.pbAlan.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pbAlan_MouseMove);
+            this.pbAlan.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pbAlan_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pbAlan);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbAlan)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbAlan;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proje5._36_PuzzleOyunu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Bitmap resim = null, arkaPlan = null, tahta = null;
+        private List<Parca> parcalar = null;
+        int satirSayisi, kolonSayisi, satirYuksekligi, kolonGenisligi;
+        private Parca hareketEdenParca = null;
+        private Point hareketNoktasi;
+        private bool oyunBitisi = true;
+        private OpenFileDialog ofdDosyaSecimi = new OpenFileDialog();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if(ofdDosyaSecimi.ShowDialog() == DialogResult.OK)
+            {
+                Bitmap bm = new Bitmap(ofdDosyaSecimi.FileName);
+                resim = new Bitmap(bm.Width, bm.Height);
+                Graphics gr = Graphics.FromImage(resim);
+                gr.DrawImage(bm, 0, 0, bm.Width, bm.Height);
+
+                arkaPlan = new Bitmap(resim.Width, resim.Height);
+                tahta = new Bitmap(resim.Width, resim.Height);
+                pbAlan.Size = resim.Size;
+                pbAlan.Image = tahta;
+                this.ClientSize = new Size(pbAlan.Right + pbAlan.Left, pbAlan.Bottom + pbAlan.Left);
+
+                if (resim == null) return;
+                oyunBitisi = false;
+
+                satirSayisi = 5;
+                satirYuksekligi = resim.Height / satirSayisi;
+                kolonSayisi = 5;
+                kolonGenisligi = resim.Width / kolonSayisi;
+
+                Random rand = new Random();
+                parcalar =  new List<Parca>();
+                for (int row = 0; row < satirSayisi; row++)
+                {
+                    int hgt = satirYuksekligi;
+                    if (row == satirSayisi - 1) hgt = resim.Height - row * satirYuksekligi;
+                    for (int col = 0; col < kolonSayisi; col++)
+                    {
+                        int wid = kolonGenisligi;
+                        if(col == kolonSayisi - 1) wid = resim.Width - col * kolonGenisligi;
+                        Rectangle rect = new Rectangle(col * kolonGenisligi, row * satirYuksekligi,
+                            wid, hgt);
+
+                        Parca yeniParca = new Parca(resim, rect);
+
+                        yeniParca.MevcutNoktasi = new Rectangle(rand.Next(0, resim.Width - wid), rand.Next(0, resim.Height - hgt), wid, hgt);
+
+                        parcalar.Add(yeniParca);
+                    }
+                }
+                arkaPlanYap();
+                tahtaOlustur();
+            }
+        }
+
+        private void arkaPlanYap()
+        {
+            Graphics gr = Graphics.FromImage(arkaPlan);
+            gr.Clear(pbAlan.BackColor);
+            diyagramOlustur(gr);
+            parcalariOlustur(gr);
+            pbAlan.Visible = true;
+            pbAlan.Refresh();
+        }
+
+        private void diyagramOlustur(Graphics gr)
+        {
+            Pen kalem = new Pen(Color.DarkGray, 4);
+            for(int y = 0; y <= resim.Height; y += satirYuksekligi) gr.DrawLine(kalem, 0, y, resim.Width, y);
+
+            gr.DrawLine(kalem, 0, resim.Height, resim.Width, resim.Height);
+            for (int x = 0; x <= resim.Width; x += kolonGenisligi) gr.DrawLine(kalem, x, 0, x, resim.Height);
+
+            gr.DrawLine(kalem, resim.Width, 0, resim.Width, resim.Height);
+        }
+
+        private void parcalariOlustur(Graphics gr)
+        {
+            Pen beyazKalem = new Pen(Color.White, 3);
+            Pen siyahKalem = new Pen(Color.Black, 3);
+            foreach(Parca piece in parcalar)
+            {
+                //Sabitlenmiş parçalar dışındakiler için.
+                if(piece != hareketEdenParca)
+                {
+                    gr.DrawImage(resim,
+                        piece.MevcutNoktasi,
+                        piece.BaslangicNoktasi,
+                        GraphicsUnit.Pixel);
+                    if(!oyunBitisi)
+                    {
+                        if (piece.baslangicNoktasindaMi())
+                            gr.DrawRectangle(beyazKalem, piece.MevcutNoktasi);
+                        else
+                            //Siyah kenarlıklı kilitli parçalar çizin.
+                            gr.DrawRectangle(siyahKalem, piece.MevcutNoktasi);
+                    }
+                }
+            }
+        }
+
+        private void tahtaOlustur()
+        {
+            Graphics gr = Graphics.FromImage(tahta);
+            gr.DrawImage(arkaPlan, 0, 0, arkaPlan.Width, arkaPlan.Height);
+            if(hareketEdenParca != null)
+            {
+                gr.DrawImage(resim, hareketEdenParca.MevcutNoktasi, 
+                    hareketEdenParca.BaslangicNoktasi, GraphicsUnit.Pixel);
+                Pen blue_pen = new Pen(Color.Blue, 4);
+                gr.DrawRectangle(blue_pen, hareketEdenParca.MevcutNoktasi);
+            }
+            pbAlan.Visible = true;
+            pbAlan.Refresh();
+        }
+
+        private void pbAlan_MouseDown(object sender, MouseEventArgs e)
+        {
+            hareketEdenParca = null;
+            foreach(Parca piece in parcalar)
+            {
+                if(!piece.baslangicNoktasindaMi() && piece.Contains(e.Location))
+                    hareketEdenParca = piece;
+            }
+            if (hareketEdenParca == null) return;
+
+            hareketNoktasi = e.Location;
+
+            parcalar.Remove(hareketEdenParca);
+            parcalar.Add(hareketEdenParca);
+
+            arkaPlanYap();
+            tahtaOlustur();
+        }
+
+        private void pbAlan_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (hareketEdenParca == null) return;
+
+            int dx = e.X - hareketNoktasi.X;
+            int dy = e.Y - hareketNoktasi.Y;
+            hareketEdenParca.MevcutNoktasi.X += dx;
+            hareketEdenParca.MevcutNoktasi.Y += dy;
+
+            hareketNoktasi = e.Location;
+
+            tahtaOlustur();
+        }
+
+        private void pbAlan_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (hareketEdenParca == null) return;
+
+            if(hareketEdenParca.baslangicNoktasinaYapistir())
+            {
+                parcalar.Remove(hareketEdenParca);
+                parcalar.Reverse();
+                parcalar.Add(hareketEdenParca);
+                parcalar.Reverse();
+
+                oyunBitisi = true;
+                foreach(Parca piece in parcalar)
+                {
+                    if(!piece.baslangicNoktasindaMi())
+                    {
+                        oyunBitisi = false;
+                        break;
+                    }
+                }
+            }
+
+            hareketEdenParca = null;
+
+            arkaPlanYap();
+            tahtaOlustur();
+        }
+    }
+
+    class Parca
+    {
+        public Bitmap resim;
+        public Rectangle BaslangicNoktasi, MevcutNoktasi;
+
+        public Parca(Bitmap yeniResim, Rectangle baslangicYeri)
+        {
+            resim = yeniResim;
+            BaslangicNoktasi = baslangicYeri;
+        }
+
+        public bool baslangicNoktasindaMi()
+        {
+            return BaslangicNoktasi.Equals(MevcutNoktasi);
+        }
+
+        public bool Contains(Point p)
+        {
+            return MevcutNoktasi.Contains(p);
+        }
+
+        public bool baslangicNoktasinaYapistir()
+        {
+            if((Math.Abs(MevcutNoktasi.X - BaslangicNoktasi.X) < 20) &&
+               (Math.Abs(MevcutNoktasi.Y - BaslangicNoktasi.Y) < 20))
+            {
+                MevcutNoktasi = BaslangicNoktasi;
+                return true;
+            }
+            return false;
+        }
+    }
+}
